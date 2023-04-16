d9be7bd87e5c1493d454f3645e35b8b9decf1d65
samaygodika <samgodika@gmail.com>
2023-04-16 22:32:09 +0000

    closest segment

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -39,15 +39,32 @@ class PurePursuit(object):
     def odom_callback(self, odom):
         self.pose = odom.pose.pose
         current_robot_position = np.array([self.pose.position.x, self.pose.position.y])
-        current_traj_point = np.array([self.trajectory.points[0], self.trajectory.points[1]])
-        projection, distance = self.minimum_distance(current_traj_point[0], current_traj_point[1], current_robot_position)
+        #current_traj_point = np.array([self.trajectory.points[0], self.trajectory.points[1]])
+        projection, distance = self.min_distance(self.trajectory.points, current_robot_position)
         #publish projection onto rviz 
         point = Point(x = projection[0], y = projection[1], z = 0)
         self.nearest_pt_sub.publish(PointStamped(point = point, header = Header(frame_id = "map")))
 
+    def min_distance(self, points, position):
+        start_points = np.array(points)
+        #to get distances between every adjacent pair of points
+        end_points = np.roll(start_points, -1, axis=0)
+        start_points = start_points[:-1]
+        end_points = end_points[:-1]
+        l2 = np.linalg.norm(start_points - end_points, axis=1)**2 #distances between every two pair of points 
+        #put a condition to make l2 = l2 + epsilon wherever it is equal to 0
+        l2 = np.where(l2 == 0, 0.001, l2)
+        t = np.clip(np.sum((position - start_points) * (end_points - start_points), axis=1)/l2, 0, 1)
+        projection = start_points + (t * (end_points - start_points).T).T
+        distance = np.linalg.norm(position - projection, axis=1)
+        minimum_dist_index = np.argmin(distance)
+        min_distance = distance[minimum_dist_index]
+        min_point = projection[minimum_dist_index]
+        return min_point, min_distance
+
     def minimum_distance(self, v, w, p):
         #v, w are your start points and end points 
-        #p is your current position 
+        #p is your current position
 
         l2 = np.linalg.norm(w-v)**2 #|v-w|^2 
         if l2 == 0.0: return np.linalg.norm(w-v)
