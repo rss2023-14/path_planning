de2c58bda14b7af8d4e45d427a2bbb265bb986d8
samaygodika <samgodika@gmail.com>
2023-04-17 04:24:38 +0000

    finding lookahead point

--- src/pure_pursuit.py
+++ src/pure_pursuit.py
@@ -17,7 +17,7 @@ class PurePursuit(object):
     """
     def __init__(self):
         self.odom_topic       = rospy.get_param("~odom_topic", "pf/pose/odom")
-        # self.lookahead        = # FILL IN 
+        self.lookahead        = 1.0 # FILL IN 
         # self.speed            = # FILL IN 
         # self.wheelbase_length = # FILL IN 
         self.pose = None
@@ -40,10 +40,15 @@ class PurePursuit(object):
         self.pose = odom.pose.pose
         current_robot_position = np.array([self.pose.position.x, self.pose.position.y])
         #current_traj_point = np.array([self.trajectory.points[0], self.trajectory.points[1]])
-        projection, distance = self.min_distance(self.trajectory.points, current_robot_position)
+        projection, min_dist_index = self.min_distance(self.trajectory.points, current_robot_position)
+        lookahead = self.find_lookahead(min_dist_index, current_robot_position)
         #publish projection onto rviz 
-        point = Point(x = projection[0], y = projection[1], z = 0)
-        self.nearest_pt_sub.publish(PointStamped(point = point, header = Header(frame_id = "map")))
+        if lookahead is not None:
+            point = Point(x = lookahead[0], y = lookahead[1], z = 0)
+            self.nearest_pt_sub.publish(PointStamped(point = point, header = Header(frame_id = "map")))
+        else:
+            #print('nothing found')
+            pass
 
     def min_distance(self, points, position):
         start_points = np.array(points)
@@ -52,7 +57,7 @@ class PurePursuit(object):
         start_points = start_points[:-1]
         end_points = end_points[:-1]
         l2 = np.linalg.norm(start_points - end_points, axis=1)**2 #distances between every two pair of points 
-        #put a condition to make l2 = l2 + epsilon wherever it is equal to 0
+        #put a condition to make l2 = epsilon wherever it is equal to 0
         l2 = np.where(l2 == 0, 0.001, l2)
         t = np.clip(np.sum((position - start_points) * (end_points - start_points), axis=1)/l2, 0, 1)
         projection = start_points + (t * (end_points - start_points).T).T
@@ -60,7 +65,38 @@ class PurePursuit(object):
         minimum_dist_index = np.argmin(distance)
         min_distance = distance[minimum_dist_index]
         min_point = projection[minimum_dist_index]
-        return min_point, min_distance
+        return min_point, minimum_dist_index
+    
+    def find_lookahead(self, minimum_dist_index, position):
+        current_index = minimum_dist_index
+        Q = position
+        r = self.lookahead
+
+        while True:
+            P1 = np.array(self.trajectory.points[current_index])
+            P2 = np.array(self.trajectory.points[current_index + 1])
+            V = P2 - P1
+            a = V.dot(V)
+            b = 2 * V.dot(P1 - Q)
+            c = P1.dot(P1) + Q.dot(Q) - 2 * P1.dot(Q) - r**2
+            disc = b**2 - 4*a*c 
+            if disc < 0: #no real solutions
+                current_index += 1
+                if current_index >= len(self.trajectory.points) - 1:
+                    return None
+                continue
+            
+            sqrt_disc = disc ** 0.5
+            t1 = (-b + sqrt_disc) / (2*a)
+
+            if not (0 <= t1 <= 1):
+                #print('missed line segment')
+                current_index += 1
+                if current_index >= len(self.trajectory.points) - 1:
+                    return None
+                continue
+            else:
+                return P1 + t1 * V
 
     def minimum_distance(self, v, w, p):
         #v, w are your start points and end points 
