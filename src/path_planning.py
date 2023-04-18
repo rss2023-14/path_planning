d1457ce55e69ca591ca7596ddce47fc89a205a16
Ian Gatlin <iagatlin@mit.edu>
2023-04-18 12:37:01 -0400

    separated PRM graph creation as new function

--- src/path_planning.py
+++ src/path_planning.py
@@ -52,106 +52,104 @@ class PathPlan(object):
         msg.pose.position  # x, y, z
         msg.pose.orientation  # x, y, z, w
 
-    def plan_path(self, start_point, end_point, map):
-        if True:
-            ## Assume we have heuristic function called 'heuristic' which takes in (start,end)
-            # Assume map is a dictionary of nodes and each of their neighbors with associated distance in a len 2 tuple
+    def create_sampled_graph(self, start_point, end_point, map): # sampling based method / PRM
+        width = self.occupancy[0].size # self.occupancy is a 2d numpy array where array[y][x]
+        height = self.occupancy.size / width; # should be integer
 
-            # Define a function to calculate the Manhattan distance between two points
-            def heuristic(start, end):
-                return abs(start[0] - end[0]) + abs(start[1] - end[1])
+        points = [start_point, end_point] # put start and end point in the list in the form of (width, height) tuples
+        for i in range(100): # find random points
+            x_rand_point = numpy.random.randint(0, width-1)
+            y_rand_point = numpy.random.randint(0, height-1)
+            points.append((x_rand_point, y_rand_point))
 
-            # Define a dictionary to keep track of the cost of reaching each node from the start node
-            g_scores = {start_point: 0}
+        valid_points = [] 
+        for x, y in points: # make sure sampled points are not inside obstacles
+            if self.occupancy[y, x] == True: # True means they are not inside obstacle
+                valid_points.append((x, y))
 
-            # Define a dictionary to keep track of the parent node for each visited node
-            parents = {start_point: None}
-
-            # Create a set to keep track of the visited nodes
-            visited = set()
-
-            # Create a list to store the open nodes and their f-scores
-            open_nodes = [(heuristic(start_point, end_point), start_point)]
-
-            # Loop until we find the goal node or exhaust all possible paths
-            while open_nodes:
-                # Sort the open nodes by their f-scores (which is the sum of the g-score and the heuristic estimate)
-                open_nodes.sort()
-
-                # Get the node with the lowest f-score from the list of open nodes
-                current = open_nodes.pop(0)[1]
-
-                # If we have found the goal node, reconstruct the path and return it
-                if current == end_point:
-                    path = []
-                    while current:
-                        path.append(current)
-                        current = parents[current]
-                    path.reverse()
-                    return path
-
-                # Add the current node to the visited set
-                visited.add(current)
-
-                # Loop through the current node's neighbors
-                for neighbor, distance in map[current]:
-                    # If we have already visited this neighbor, skip it
-                    if neighbor in visited:
-                        continue
-
-                    # Calculate the tentative g-score for this neighbor
-                    tentative_g_score = g_scores[current] + distance
-
-                    # If we have not yet visited this neighbor, or if we have found a shorter path to it,
-                    # update its g-score and add it to the list of open nodes
-                    if neighbor not in g_scores or tentative_g_score < g_scores[neighbor]:
-                        g_scores[neighbor] = tentative_g_score
-                        f_score = tentative_g_score + heuristic(neighbor, end_point)
-                        open_nodes.append((f_score, neighbor))
-                        parents[neighbor] = current
-
-            # If we have exhausted all possible paths and have not found the goal node, return None
-            return None
-        
-        else: # sampling based method / PRM
-            width = self.occupancy[0].size # self.occupancy is a 2d numpy array where array[y][x]
-            height = self.occupancy.size / width;
-
-            points = [] # find random points
-            for i in range(100):
-                width = numpy.random.randint(0, width-1)
-                height = numpy.random.randint(0, height-1)
-                points.append((width, height))
-
-            valid_points = [] # make sure they are valid
-            for x, y in points:
-                if self.occupancy[y, x] == True:
-                    valid_points.append((x, y))
+        assert(start_point in valid_points) # make sure start_point & end_point are in correct form
+        assert(end_point in valid_points)
             
-            adjacency_graph = {point: [] for point in valid_points}
-            for index, first_point in enumerate(valid_points): # draw edges
-                for second_point in valid_points[index+1:]:
-                    slope = float(second_point[1] - first_point[1]) / (second_point[0] - first_point[0]) # find equation for line
-                    b = first_point[1] - slope * first_point[0]
+        adjacency_graph = {point: [] for point in valid_points}
+        for index, first_point in enumerate(valid_points): # find all permutations of points
+            for second_point in valid_points[index+1:]:
+                slope = float(second_point[1] - first_point[1]) / (second_point[0] - first_point[0]) # find equation for line
+                b = first_point[1] - slope * first_point[0]
 
-                    isCollision = False
-                    for x_val in range(first_point[0], second_point[0]):
-                        y_val = slope * x_val + b
-                        if self.occupancy[y_val, x_val] == False:
-                            isCollision = True
-                            break
-                    
-                    if not isCollision:
-                        adjacency_graph[first_point].append(second_point)
-                        adjacency_graph[second_point].append(first_point)
+                isCollision = False
+                for x_val in range(first_point[0], second_point[0]): # check to see if any discrete x values of line are in bad area
+                    y_val = slope * x_val + b
+                    if self.occupancy[y_val, x_val] == False: # if one of the points on the line is in an obstacle, dont add to adjacency graph
+                        isCollision = True
+                        break
+                
+                if not isCollision:
+                    adjacency_graph[first_point].append(second_point)
+                    adjacency_graph[second_point].append(first_point)
 
 
-            return adjacency_graph
-                    # find equation of line
-                    # check to see if any discrete x values of line are in bad area
+        return adjacency_graph
 
-            return None
+    def plan_path(self, start_point, end_point, map):
+        ## Assume we have heuristic function called 'heuristic' which takes in (start,end)
+        # Assume map is a dictionary of nodes and each of their neighbors with associated distance in a len 2 tuple
 
+        # Define a function to calculate the Manhattan distance between two points
+        def heuristic(start, end):
+            return abs(start[0] - end[0]) + abs(start[1] - end[1])
+
+        # Define a dictionary to keep track of the cost of reaching each node from the start node
+        g_scores = {start_point: 0}
+
+        # Define a dictionary to keep track of the parent node for each visited node
+        parents = {start_point: None}
+
+        # Create a set to keep track of the visited nodes
+        visited = set()
+
+        # Create a list to store the open nodes and their f-scores
+        open_nodes = [(heuristic(start_point, end_point), start_point)]
+
+        # Loop until we find the goal node or exhaust all possible paths
+        while open_nodes:
+            # Sort the open nodes by their f-scores (which is the sum of the g-score and the heuristic estimate)
+            open_nodes.sort()
+
+            # Get the node with the lowest f-score from the list of open nodes
+            current = open_nodes.pop(0)[1]
+
+            # If we have found the goal node, reconstruct the path and return it
+            if current == end_point:
+                path = []
+                while current:
+                    path.append(current)
+                    current = parents[current]
+                path.reverse()
+                return path
+
+            # Add the current node to the visited set
+            visited.add(current)
+
+            # Loop through the current node's neighbors
+            for neighbor, distance in map[current]:
+                # If we have already visited this neighbor, skip it
+                if neighbor in visited:
+                    continue
+
+                # Calculate the tentative g-score for this neighbor
+                tentative_g_score = g_scores[current] + distance
+
+                # If we have not yet visited this neighbor, or if we have found a shorter path to it,
+                # update its g-score and add it to the list of open nodes
+                if neighbor not in g_scores or tentative_g_score < g_scores[neighbor]:
+                    g_scores[neighbor] = tentative_g_score
+                    f_score = tentative_g_score + heuristic(neighbor, end_point)
+                    open_nodes.append((f_score, neighbor))
+                    parents[neighbor] = current
+
+        # If we have exhausted all possible paths and have not found the goal node, return None
+        return None
+        
         # publish trajectory
         self.traj_pub.publish(self.trajectory.toPoseArray())
 
