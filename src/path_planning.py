af692fe2eb176736ba902cee5d324b91b279b309
joposada7 <joposada7@gmail.com>
2023-04-21 06:21:53 -0400

    update documentation

--- src/path_planning.py
+++ src/path_planning.py
@@ -40,41 +40,45 @@ class PathPlan(object):
         self.trajectory = LineTrajectory("/planned_trajectory")
         self.traj_pub = rospy.Publisher("/trajectory/current", PoseArray, queue_size=10)
 
-    def map_cb(self, msg):  # Occupancy Grid
-        # msg.data  # int array, row major order, starting with 0,0, probabilties in range of 0, 100
-        # msg.info.width  # int
-        # msg.info.height  # int
-        # msg.info.origin.position  # x, y, z
-        # msg.info.origin.orientation  # x, y, z, w
+    def map_cb(self, msg):
+        """
+        Gets OccupancyGrid map and constructs graph.
+        """
+        # skimage.io.imsave("stata_basement_occupancy.png", msg.data)
+
         o = msg.info.origin.orientation
         theta = euler_from_quaternion([o.x, o.y, o.z, o.w])[2]
         sin_val = np.sin(theta)
         cos_val = np.cos(theta)
 
+        # Get map info
+        self.resolution = msg.info.resolution
+        p = msg.info.origin.position
+        self.translation = np.array([p.x, p.y, p.z])
         self.rot_matrix = np.matrix(
             [[cos_val, -sin_val, 0.0], [sin_val, cos_val, 0.0], [0.0, 0.0, 1.0]]
         )
         self.inv_rot_matrix = np.linalg.inv(self.rot_matrix)
-        self.resolution = msg.info.resolution
-        p = msg.info.origin.position
-        self.translation = np.array([p.x, p.y, p.z])
 
-        # skimage.io.imsave("stata_basement_occupancy.png", msg.data)
+        # Make graph
         (self.graph, self.occupancy) = self.make_occupancy_graph(
             np.array(msg.data), msg.info.width, msg.info.height
         )
         rospy.loginfo("Initialized, graph made!")
 
-    def odom_cb(self, msg):  # Odometry
+    def odom_cb(self, msg):
+        """
+        Gets Odometry msg and stores current pose.
+        """
         p = msg.pose.pose.position  # x, y, z
         o = msg.pose.pose.orientation  # x, y, z, w
         t = msg.twist
 
         self.current_pose = (p.x, p.y)  # Only do (x,y)
 
-    def goal_cb(self, msg):  # PoseStamped
+    def goal_cb(self, msg):
         """
-        Get goal point, and initiate path planning to follow!
+        Get goal point as PoseStamped, and initiate path planning to follow.
         """
         if self.current_pose is None:
             rospy.loginfo("Odom not initialized yet!")
@@ -83,59 +87,59 @@ class PathPlan(object):
             rospy.loginfo("Graph not initialized yet!")
             return
 
+        # Start
         sp = self.current_pose
         so = (0, 0, 0, 0)
+
+        # Goal
         gp = msg.pose.position  # x, y, z
         go = msg.pose.orientation  # x, y, z, w
 
         path = self.plan_path(sp, (gp.x, gp.y), self.graph)
 
-    def create_sampled_graph(  # returns a graph in the form { (x1, y1) : [(x2, y2), (x3, y3)] }
-        self, start_point, end_point, map
-    ):  # sampling based method / PRM
-        width = self.occupancy[
-            0
-        ].size  # self.occupancy is a 2d numpy array where array[y][x]
+    def create_sampled_graph(self, start_point, end_point, map):
+        """
+        Create graph using PRM/sampling-based method.
+        Returns a graph in form {(x1, y1): [(x2, y2), (x3, y3)]}
+        """
+        # Get occupancy dimensions (2d numpy array[y][x]), integers
+        width = self.occupancy[0].size
         height = self.occupancy.size / width
-        # should be integer
 
-        points = [
-            start_point,
-            end_point,
-        ]  # put start and end point in the list in the form of (width, height) tuples
-        for i in range(100):  # find random points
+        # Initialize start and end in list of (width, height) tuples
+        points = [start_point, end_point]
+
+        # Find random points
+        for i in range(100):
             x_rand_point = np.random.randint(0, width - 1)
             y_rand_point = np.random.randint(0, height - 1)
             points.append((x_rand_point, y_rand_point))
 
         valid_points = []
-        for x, y in points:  # make sure sampled points are not inside obstacles
-            if self.occupancy[y, x] == True:  # True means they are not inside obstacle
+        for x, y in points:
+            # Make sure sampled points are not inside obstacles
+            if self.occupancy[y, x]:
+                # True means they are not inside obstacle
                 valid_points.append((x, y))
 
-        assert (
-            start_point in valid_points
-        )  # make sure start_point & end_point are in correct form
-        assert end_point in valid_points
+        # Make sure start and end points are in correct form
+        assert start_point in valid_points, "Start point not in correct form!"
+        assert end_point in valid_points, "End point not in correct form!"
 
+        # Find all permutations of points
         adjacency_graph = {point: [] for point in valid_points}
-        for index, first_point in enumerate(
-            valid_points
-        ):  # find all permutations of points
+        for index, first_point in enumerate(valid_points):
             for second_point in valid_points[index + 1 :]:
-                slope = float(second_point[1] - first_point[1]) / (
-                    second_point[0] - first_point[0]
-                )  # find equation for line
+                # Find equation for line
+                slope = float(second_point[1] - first_point[1]) / (second_point[0] - first_point[0])
                 b = first_point[1] - slope * first_point[0]
 
+                # Check for collision
                 isCollision = False
-                for x_val in range(
-                    first_point[0], second_point[0]
-                ):  # check to see if any discrete x values of line are in bad area
+                for x_val in range(first_point[0], second_point[0]):
                     y_val = slope * x_val + b
-                    if (
-                        self.occupancy[y_val, x_val] == False
-                    ):  # if one of the points on the line is in an obstacle, dont add to adjacency graph
+                    if not self.occupancy[y_val, x_val]:
+                        # If any point on the line is in an obstacle, don't add to adjacency graph
                         isCollision = True
                         break
 
@@ -146,15 +150,20 @@ class PathPlan(object):
         return adjacency_graph
 
     def plan_path(self, start_point, end_point, map):
-        ## Assume we have heuristic function called 'heuristic' which takes in (start,end)
-        # Assume map is a dictionary of nodes and each of their neighbors with associated distance in a len 2 tuple
+        """
+        Take graph and use A* planning to find best path according to heuristic function.
+            - Heuristic takes (start, end) nodes
+            - Assumes map is a nested dictionary of neighbors {(x1, y1): {(x2, y2): distance}}
+        """
         rospy.loginfo("Planning path...")
 
         # Define a function to calculate the Manhattan distance between two points
         def heuristic(start, end):
             return abs(start[0] - end[0]) + abs(start[1] - end[1])
+
         self.add_node(start_point[0],start_point[1])
         self.add_node(end_point[0],end_point[1])
+
         # Define a dictionary to keep track of the cost of reaching each node from the start node
         g_scores = {start_point: 0}
 
@@ -170,7 +179,6 @@ class PathPlan(object):
         # Loop until we find the goal node or exhaust all possible paths
         while open_nodes:
             open_nodes.sort()
-
             current = open_nodes.pop(0)[1]
 
             # If we have found the goal node, reconstruct the path and return it
@@ -180,11 +188,11 @@ class PathPlan(object):
                     path.append(current)
                     current = parents[current]
                 path.reverse()
-                # publish trajectory
-                self.traj_pub.publish(self.trajectory.toPoseArray())
 
-                # visualize trajectory Markers
+                # Publish and visualize trajectory
+                self.traj_pub.publish(self.trajectory.toPoseArray())
                 self.trajectory.publish_viz()
+
                 rospy.loginfo("Path found: " + str(path))
                 return path
 
@@ -208,22 +216,22 @@ class PathPlan(object):
                     open_nodes.append((f_score, neighbor))
                     parents[neighbor] = current
 
-        # return none if no paths are found
-        # publish trajectory
+        # No path found
         self.traj_pub.publish(self.trajectory.toPoseArray())
-
-        # visualize trajectory Markers
         self.trajectory.publish_viz()
         return None
 
     def pixel_to_world(self, x, y):
-        """takes position in image frame and returns same position in world frame"""
+        """
+        Takes position in image frame and returns same position in world frame.
+        """
         result = np.dot(self.inv_rot_matrix, ([x, y, 0] - self.translation))
-
         return (result[0, 0] / self.resolution, result[0, 1] / self.resolution)
 
     def world_to_pixel(self, x, y):
-        """takes position in world frame and returns same position in pixel frame"""
+        """
+        Takes position in world frame and returns same position in pixel frame.
+        """
         result = (
             np.dot(self.rot_matrix, [x * self.resolution, y * self.resolution, 0])
             + self.translation
@@ -232,7 +240,8 @@ class PathPlan(object):
         return (result[0, 0], result[0, 1])
 
     def make_occupancy_graph(self, data, width, height):
-        """create an eroded occupancy grid and graph of the map
+        """
+        Create an eroded occupancy grid and graph of the map.
 
         Args:
             data (1d array): occupancy grid data
@@ -242,7 +251,7 @@ class PathPlan(object):
         Returns:
             (dict of dicts, 2d array): graph and eroded occupancy grid
         """
-
+        # Reset occupancy grid values
         for i in range(len(data)):
             if data[i] == -1:
                 data[i] = 100
@@ -251,33 +260,30 @@ class PathPlan(object):
             else:
                 data[i] = 0
 
+        # Construct image
         img = skimage.color.rgb2gray(np.array(data).reshape(height, width))
-
         width = len(img[0])
         height = len(img)
 
-        ## add in the start and end nodes
-
-        # print(width, height)
-
+        # Erode occupancy grid
         globalthreshold = skimage.filters.threshold_otsu(img)
         occupancy_grid = img > globalthreshold
         footprint = skimage.morphology.disk(10)
         occupancy_grid = skimage.morphology.erosion(occupancy_grid, footprint)
 
-        # get random sample of valid pixel locations
+        # Get random sample of valid pixel locations
         vertices = {(834, 527)}
         while len(vertices) < 1000:
             (x, y) = (random.randrange(width), random.randrange(height))
             if occupancy_grid[y][x] == False:
                 vertices.add((x, y))
 
-        # create graph from vertices
+        # Create graph from vertices
         adjacency = {}
         for x, y in vertices:
             adjacency[(x, y)] = self.find_nearest_nodes(x, y, vertices, occupancy_grid, 20)
 
-        # transform to world frame
+        # Transform to world frame
         world_frame_adj = {}
         for x, y in adjacency:
             adjacent = {}
@@ -287,21 +293,21 @@ class PathPlan(object):
                 )
             world_frame_adj[self.pixel_to_world(x, y)] = adjacent
 
-        # print(adjacency)
-        # skimage.io.imsave("stata_basement_thresh.png", occupancy_grid)
         return (world_frame_adj, occupancy_grid)
 
     def add_node(self, x, y):
+        """
+        Add node (x,y) to graph, connect edges to closest neighbors
+        """
         nearest = self.find_nearest_nodes(x, y, self.graph, self.occupancy, 20, True)
-
         self.graph[(x, y)] = nearest
 
         for node in nearest:
             self.graph[node][(x, y)] = nearest[node]
 
-
     def find_nearest_nodes(self, x, y, vertices, occupancy_grid, num_taken, world_frame=False):
-        """finds nearest valid nodes
+        """
+        Find nearest valid nodes in graph to (x,y).
 
         Args:
             x, y (float): x and y position
@@ -331,9 +337,9 @@ class PathPlan(object):
             if world_frame:
                 (x_img, y_img) = self.world_to_pixel(x, y)
                 (i_img, j_img) = self.world_to_pixel(i, j)
-                line_pixels = create_line(int(x_img), int(y_img), int(i_img), int(j_img))
+                line_pixels = self.create_line(int(x_img), int(y_img), int(i_img), int(j_img))
             else:
-                line_pixels = create_line(x, y, i, j)
+                line_pixels = self.create_line(x, y, i, j)
             for x_pos, y_pos in line_pixels:
                 if occupancy_grid[y_pos][x_pos] == False:
                     remove_these.add((i, j))
@@ -344,26 +350,26 @@ class PathPlan(object):
 
         return nearest
 
+    def create_line(self, x, y, i, j):
+        """
+        Find points between two points (x,y) and (i,j).
 
-def create_line(x, y, i, j):
-    """finds points between two points
+        Args:
+            x, y (float): pos 1
+            i, j (float): pos 2
 
-    Args:
-        x, y (float): pos 1
-        i, j (float): pos 2
+        Returns:
+            set: points between
+        """
+        pixels = set()
+        for x_val in range(x, i):
+            y_found = (((j - y) / (i - x)) * (x_val - x)) + y
+            pixels.add((x_val, int(y_found)))
+        for y_val in range(y, j):
+            x_found = (((i - x) / (j - y)) * (y_val - y)) + x
+            pixels.add((int(x_found), y_val))
 
-    Returns:
-        set: points between
-    """
-    pixels = set()
-    for x_val in range(x, i):
-        y_found = (((j - y) / (i - x)) * (x_val - x)) + y
-        pixels.add((x_val, int(y_found)))
-    for y_val in range(y, j):
-        x_found = (((i - x) / (j - y)) * (y_val - y)) + x
-        pixels.add((int(x_found), y_val))
-
-    return pixels
+        return pixels
 
 
 if __name__ == "__main__":
